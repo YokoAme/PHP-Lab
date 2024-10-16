d5f01c9cd191d8a1078556753b289fbc66afd904
Masha <yawarakairairakku@gmail.com>
2024-10-16 10:42:58 +0300

    Решила задачу 8 уровня

--- Labs.php
+++ Labs.php
@@ -1,4 +1,11 @@
 <?php>
-int Func(int age){
-    
+//8 уровень заданий
+
+// Square(n) Sum
+function square_sum($numbers) : int {
+    $sum=0;
+    foreach($numbers as $num){
+      $sum+=$num*$num;
+    }
+    return $sum;
 }
